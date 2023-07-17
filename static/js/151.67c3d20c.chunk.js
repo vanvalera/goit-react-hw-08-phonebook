21fbf13765fb9e6e501f243b76142345dbfb4ddc
vanvalera <115824927+vanvalera@users.noreply.github.com>
2023-07-17 19:06:43 +0000

    Deploying to gh-pages from @ vanvalera/goit-react-hw-08-phonebook@879a93720df054969c2121727d6bc897f93557ea 🚀

---
+++ static/js/151.67c3d20c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[151],{3151:function(e,t,o){o.r(t),o.d(t,{default:function(){return c}});var n="Home_home__uEIM2",r=o(3329);function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{className:n,children:"Welcome to phonebook app!"})})}}}]);
+//# sourceMappingURL=151.67c3d20c.chunk.js.map
